7bddd88ee610d34a45a62940b014748ffedfba05
tianhe <ittianshi@gmail.com>
2017-02-27 15:28:14 +0800

    OK

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from flask import Flask, url_for
+from flask import Flask, url_for,request
+import hashlib
+
+
 app = Flask(__name__)
 
-@app.route('/wx')
+@app.route('/wx', methods=['POST'])
 def wx():
-    return "hello is a test"
+    print(request.form.get("a"))
+    print(request.form.get("a"))
+    print(request.form.get("a"))
+    print(request.data)
+    print(request.args)
+    print(request.base_url)
+    print(request.cookies)
+
+    return "hei"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
