99fa33f1eca82f4314da458d54a10b97076919af
tianhe <ittianshi@gmail.com>
2017-02-27 16:41:35 +0800

    提交信息

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, url_for,request
 import hashlib
-
-
+import xml.etree.ElementTree as ET
+import receive
 app = Flask(__name__)
-
+import reply
 @app.route('/', methods=['GET'])
 def wx():
     print(request.form.get("a"))
@@ -17,6 +17,18 @@ def wx():
 
     return text
 
+@app.route('/', methods=['POST'])
+def wx2():
+    xmlData = request.data;
+    print(xmlData)
+    recMsg = receive.parse_xml(xmlData);
+    if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+        toUser = recMsg.FromUserName
+        fromUser = recMsg.ToUserName
+        content = "test"
+        replyMsg = reply.TextMsg(toUser, fromUser, content)
+        return replyMsg.send()
+    return ""
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0',port=80)
